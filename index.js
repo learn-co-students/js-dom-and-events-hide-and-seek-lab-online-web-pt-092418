d129c83e6846e99c786bf1f5406567f43c1e6b10
ghankeliu2015 <hanke.liu@gmail.com>
2019-04-23 16:27:20 -0400

    Done.

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ function nestedTarget(){
 }
 
 
-function deepestChild() {
-  document.querySelector
-}
-
 function increaseRankBy(n) {
   let lists = document.querySelectorAll(".ranked-list");
 
@@ -20,7 +16,17 @@ function increaseRankBy(n) {
     for (let j = 0; j < children.length; j++){
       children[j].innerHTML = parseInt(children[j].innerHTML) + n;
     }
-
   }
-
 } //end of function
+
+  function deepestChild() {
+    let node = document.getElementById('grand-node')
+    let childnode = node.children[0]
+
+    while (childnode) {
+
+      node = childnode
+      childnode = node.children[0]
+    }
+    return node
+  }
